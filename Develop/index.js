a44f654ea72f0f4e8d0ef6f16e2c42af99c5986f
tonytran97 <tonytran_97@live.com>
2022-08-30 12:59:48 -0700

    cloned repo, updated inquirer, array of questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,48 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is your project title?',
+        name: 'title',
+      },
+      {
+        type: 'input',
+        message: 'Provide a short description of your project?',
+        name: 'description',
+      },
+      {
+        type: 'input',
+        message: 'What are your installation instructions?',
+        name: 'instructions',
+      },
+      {
+        type: 'input',
+        message: 'Insert screenshots and/or demonstrations here',
+        name: 'usage',
+      },
+      {
+        type: 'input',
+        message: 'What are your contribution guidelines?',
+        name: 'contribution',
+      },
+      {
+        type: 'input',
+        message: 'What are your test instructions',
+        name: 'test',
+      },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+    .prompt(questions);
+}
 
 // Function call to initialize app
 init();
