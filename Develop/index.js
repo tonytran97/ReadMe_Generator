1d96c301ea580286c0e9e356cba65d9af4d4f8ad
tonytran97 <tonytran_97@live.com>
2022-08-30 17:50:08 -0700

    write README function

--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,6 @@
 // TODO: Include packages needed for this application
-const inquirer = require("inquirer");
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -36,12 +37,19 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, () => {
+      console.log('success');
+    });
+}
 
 // TODO: Create a function to initialize app
 function init() {
     inquirer
-    .prompt(questions);
+    .prompt(questions)
+    .then((response) =>
+    writeToFile('./README.md', JSON.stringify(response.title)));
+    
 }
 
 // Function call to initialize app
